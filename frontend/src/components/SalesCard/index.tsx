1fc5146f9efb1b27fb6dc6e07e41e8a6867b16d9
Patrik Rufino <patrik.rufino@gmail.com>
2022-11-18 12:34:08 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,6 +4,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { sale } from "../../models/sale";
 
 function SalesCard() {
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
@@ -12,10 +14,11 @@ function SalesCard() {
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
 
+  const [sales, setSales] = useState<sale[]>([]);
+
   useEffect(() => {
-    axios.get("http://localhost:8080/sales")
-    .then((response) => {
-      console.log(response.data);
+    axios.get(`${BASE_URL}/sales`).then((response) => {
+      setSales(response.data.content);
     });
   }, []);
 
@@ -55,6 +58,21 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
+            {sales.map((sale) => (
+              <tr key={sale.id}>
+                <td className="show992">{sale.id}</td>
+                <td className="show576">
+                  {new Date(sale.date).toLocaleDateString()}
+                </td>
+                <td>{sale.sellerName}</td>
+                <td className="show992">{sale.visited}</td>
+                <td className="show992">{sale.deals}</td>
+                <td>R${sale.amount.toFixed(2)}</td>
+                <td>
+                  <NotificationButton />
+                </td>
+              </tr>
+            ))}
             <tr>
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
