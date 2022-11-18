3e804084a8538547c1787deb6160b32c5c2b0c53
Patrik Rufino <patrik.rufino@gmail.com>
2022-11-18 19:01:54 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,21 +1,20 @@
 import icon from "../../assets/img/notification-icon.svg";
 import "./styles.css";
-import axios, { Axios } from 'axios';
+import axios, { Axios } from "axios";
 import { BASE_URL } from "../../utils/request";
+import { toast } from "react-toastify";
 
 type Props = {
   saleId: number;
 };
 
 function handleClick(saleId: number) {
-  axios(`${BASE_URL}/sales/${saleId}/notification`)
-    .then((response) => {
-      console.log(response);
-    }
-  );
+  axios(`${BASE_URL}/sales/${saleId}/notification`).then((response) => {
+    toast.info("SMS enviada com sucesso!");
+  });
 }
 
-function NotificationButton( { saleId }: Props) {
+function NotificationButton({ saleId }: Props) {
   return (
     <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
